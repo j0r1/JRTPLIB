663ce7fa3e18e67cc74f1da30ee49f164e452c4c
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-12-03 16:12:17 +0100

    Import of version 3.5.1

--- src/rtplibraryversion.cpp
+++ src/rtplibraryversion.cpp
@@ -35,7 +35,7 @@
 
 RTPLibraryVersion RTPLibraryVersion::GetVersion()
 {
-	return RTPLibraryVersion(3,5,0);
+	return RTPLibraryVersion(3,5,1);
 }
 
 std::string RTPLibraryVersion::GetVersionString() const
