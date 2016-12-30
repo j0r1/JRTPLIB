c5c2c406b8dec95bafc3a55e23d70a190824cabc
Jori Liesenborgs <jori.liesenborgs@gmail.com>
2016-12-30 17:32:40 +0100

    Added todo's

--- src/rtpabortdescriptors.h
+++ src/rtpabortdescriptors.h
@@ -1,3 +1,39 @@
+/*
+
+  This file is a part of JRTPLIB
+  Copyright (c) 1999-2016 Jori Liesenborgs
+
+  Contact: jori.liesenborgs@gmail.com
+
+  This library was developed at the Expertise Centre for Digital Media
+  (http://www.edm.uhasselt.be), a research center of the Hasselt University
+  (http://www.uhasselt.be). The library is based upon work done for 
+  my thesis at the School for Knowledge Technology (Belgium/The Netherlands).
+
+  Permission is hereby granted, free of charge, to any person obtaining a
+  copy of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom the
+  Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+
+*/
+
+/**
+ * \file rtpabortdescriptors.h
+ */
+
 #ifndef RTPABORTDESCRIPTORS_H
 
 #define RTPABORTDESCRIPTORS_H
@@ -8,19 +44,34 @@
 namespace jrtplib
 {
 
+/**
+ * TODO
+ */
 class RTPAbortDescriptors
 {
 public:
 	RTPAbortDescriptors();
 	~RTPAbortDescriptors();
 
+	/** TODO */
 	int Init();
+
+	/** TODO */
 	SocketType GetAbortSocket() const													{ return m_descriptors[0]; }
+
+	/** TODO */
 	bool IsInitialized() const															{ return m_init; }
+
+	/** TODO */
 	void Destroy();
 
+	/** TODO */
 	int SendAbortSignal();
+
+	/** TODO */
 	int ReadSignallingByte();
+
+	/** TODO */
 	int ClearAbortSignal();
 private:
 	SocketType m_descriptors[2];
